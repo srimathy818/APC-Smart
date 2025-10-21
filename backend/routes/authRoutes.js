0bc2663bc7f4dfcec99a7a5272f846696dbf8948
Shankaranarayanansk <sankaranarayananit002@gmail.com>
2025-10-21 15:30:04 +0530

    name fix

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,7 +1,7 @@
 import express from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import Admin from "../models/Admin.js";
+import Admin from "../models/admin.js";
 
 const router = express.Router();
 
